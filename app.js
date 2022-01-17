8698e03a8f1975b8bee5dd8207ee5caf8f32d502
whitneywind <78887901+whitneywind@users.noreply.github.com>
2022-01-17 13:41:33 -0800

    changed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // i'll eventually add something useful here
 const changeBtn = document.querySelector("#change-button");
 
-changeBtn.addEventListener("click", () => {
+changeBtn.addEventListener('click', function() {
     document.body.style.backgroundImage = "IMG_3641.jpg";
 });
