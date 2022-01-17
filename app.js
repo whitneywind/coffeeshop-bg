264bfc929fce26d875bbbd071ed3fa2606759312
whitneywind <78887901+whitneywind@users.noreply.github.com>
2022-01-17 11:56:46 -0800

    what

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // i'll eventually add something useful here
+let changeBtn = document.getElementById("change-button");
+
+changeBtn.addEventListener("click", bgChange);
+
+function bgChange() {
+    document.body.style.backgroundImage = "white";
+}
