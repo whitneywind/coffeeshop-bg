7cf7115252a1bade94ce9fa024a708d13e9c2539
whitneywind <78887901+whitneywind@users.noreply.github.com>
2022-01-21 14:58:40 -0800

    updates

--- app.js
+++ app.js
@@ -2,5 +2,9 @@
 const changeBtn = document.querySelector("#change-button");
 
 changeBtn.addEventListener('click', function() {
-    document.body.style.backgroundImage = "IMG_3641.jpg";
+    const bgOptions = ["url('IMG_3641.jpg')", "url('IMG_4140.jpg')", "url('IMG_5630.jpg')", "url('IMG_5923.jpg')", "url('IMG_7649.jpg')"];
+        document.body.style.background = bgOptions[Math.floor((Math.random() * bgOptions.length))];
+        document.body.style.backgroundPosition = "center";
+        document.body.style.backgroundSize = "cover";
+        document.body.style.height = "100vh";
 });
