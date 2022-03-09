347e13cc19b0d3632e23ebe12db458ad48c3b8c5
whitneywind <78887901+whitneywind@users.noreply.github.com>
2022-03-09 11:01:48 -0800

    made timer functional and changed font

--- app.js
+++ app.js
@@ -12,38 +12,44 @@ function readjust() {
     purposeBtns.classList.remove("initial-btns");
     welcome.style.display = "none";
     purposeQ.style.display = "none";
-    questionBox.style.margin = "0 auto";
+    questionBox.style.margin = "50px auto";
 }
 
 focusBtn.addEventListener('click', function() {
     focusContainer.style.display = "flex";
     readjust();
+    relaxBtn.style.display = "flex";
+    relaxBtn.innerHTML = "go to relax mode";
+    focusBtn.style.display = "none";
+    
 });
 
 relaxBtn.addEventListener('click', function() {
     focusContainer.style.display = "none";
     readjust();
+    focusBtn.style.display = "flex";
+    focusBtn.innerHTML = "go to focus mode";
+    relaxBtn.style.display = "none";
 });
-
-
 
 changeBtn.addEventListener('click', function() {
     const bgOptions = ["url('IMG_3641.jpg')", "url('IMG_4140.jpg')", "url('IMG_5630.jpg')", "url('IMG_5923.jpg')", "url('IMG_7649.jpg')"];
-        document.body.style.background = bgOptions[Math.floor((Math.random() * bgOptions.length))];
-        document.body.style.backgroundPosition = "center";
-        document.body.style.backgroundSize = "cover";
-        document.body.style.height = "100vh";
+        document.body.style.backgroundImage = bgOptions[Math.floor((Math.random() * bgOptions.length))];
+
 });
 
+// timer functionality
 
-
-const timeDisplay = document.querySelector("#time-display");
+const timerDisplay = document.querySelector("#timer");
 const timeSelect = document.querySelectorAll(".time-option");
 
 let countdown = 0; //variable to set/clear intervals
 let fakeDuration = 600;
 let workTime = 5;
 let seconds = 600; //seconds left on clock
+let isPaused = true;
+let startBtn = document.querySelector("#start-btn");
+let resetBtn = document.querySelector(".reset");
 
 timeSelect.forEach(option => {
     option.addEventListener('click', function() {
@@ -54,3 +60,51 @@ timeSelect.forEach(option => {
             `${Math.floor(fakeDuration / 60)}:${Math.floor(fakeDuration % 60)}`;
     });
 });
+
+startBtn.addEventListener("click", () => {
+    clearInterval(countdown);
+    isPaused = !isPaused;
+    if (!isPaused) {
+    countdown = setInterval(timer, 1000);
+    }
+})
+
+resetBtn.addEventListener("click", () => {
+    clearInterval(countdown);
+    seconds = workTime * 60;
+    countdown = 0;
+    isPaused = true;
+})
+
+function timer() {
+    seconds--;
+    if (seconds < 0) {
+        clearInterval(countdown);
+        seconds = workTime * 60;
+    }
+}
+
+function countdownDisplay() {
+    let minutes = Math.floor(seconds / 60);
+    let remainderSeconds = seconds % 60;
+    timerDisplay.textContent = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+}
+
+function buttonDisplay() {
+    if (isPaused && countdown === 0) {
+        startBtn.textContent = "start";
+    } else if (isPaused && countdown !== 0) {
+        startBtn.textContent = "continue";
+    } else {
+        startBtn.textContent = "pause";
+    }
+}
+
+function updateHTML() {
+    countdownDisplay();
+    buttonDisplay();
+}
+
+window.setInterval(updateHTML, 100);
+
+document.onclick = updateHTML;
